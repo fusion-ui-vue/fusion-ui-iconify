aede997623fc4fb9622f2022f848b4ded0a2c1ba
Shihao Xiong <62367016+sxiong5@users.noreply.github.com>
2023-10-29 16:50:20 -0400

    docs: build the website (#3)
    * feat: add material svg icons
    
    * chore: script to generate .vue
    
    * fix: rename some svg
    
    * feat: rename svg
    
    * feat: complete convert and export
    
    * regenerate vue component
    
    * docs: build the website

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,32 @@
-import { defineConfig } from "vite";
-import { resolve } from 'path';
-import path from "path";
-import vue from "@vitejs/plugin-vue";
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import PurgeIcons from 'vite-plugin-purge-icons'
-import progress from 'vite-plugin-progress'
-const root = process.cwd()
-function pathResolve(dir: string) {
-  return resolve(root, '.', dir)
-}
+import { defineConfig } from 'vite';
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-    progress(),
-    // createSvgIconsPlugin({
-    //   iconDirs: [pathResolve('src/assets/svgs')],
-    //   symbolId: 'icon-[dir]-[name]',
-    //   svgoOptions: true
-    // }),
-    // PurgeIcons(),
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tag => tag.startsWith('md-')
+        }
+      }
+    })
   ],
   build: {
     outDir: 'dist',
     lib: {
-      entry: path.resolve(__dirname, "./src/components/Icon/index.ts"),
+      entry: path.resolve(__dirname, './src/components/Icon/index.ts'),
       name: 'dist',
-      fileName: 'dist',
-    }, // 库编译模式配置
-
+      fileName: 'dist'
+    },
     rollupOptions: {
       external: ['vue'],
       output: {
         globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 });
