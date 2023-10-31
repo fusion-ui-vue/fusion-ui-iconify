5a8e59e40797f8433acd771a677be4d657b73146
ShihaoXiong <shihao.xiong@icloud.com>
2023-10-31 13:59:16 -0400

    build: npm publish v1.0.38

--- dist/install.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Component } from 'vue';
-import type { SFCInstallWithContext, SFCWithInstall } from './type';
-export declare function withInstall<T extends Component>(comp: T, name: string): SFCWithInstall<T>;
-export declare const withInstallFunction: <T>(fn: T, name: string) => SFCInstallWithContext<T>;
